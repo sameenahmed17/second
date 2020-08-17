b5fd0092b4a19a45b01609865d054300e6da250b
sameenahmed17 <sameenahmed17@gmail.com>
2020-08-17 20:57:45 +0530

    To Do List

---
+++ To-Do List Project/Assets/JS/Lib/todos.js
@@ -0,0 +1,22 @@
+$("ul").on("click", "li", function(){
+    $(this).toggleClass("completed")
+});
+
+$("ul").on("click","span",function(event){
+    $(this).parent().fadeOut(500,function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+    if (event.which === 13){
+        var texttodo =$(this).val();
+        $(this).val("");
+        $("ul").append("<li><span><i class='far fa-trash'></i></span> " + texttodo + "</li>")
+    }
+});
+
+$("#toggle-form").click(function(){
+	$("input[type='text']").fadeToggle();
+});
